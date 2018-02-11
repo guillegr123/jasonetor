73244b75d8676885deb4b44289efc381908e31e4
Guillermo Gutierrez <guillegr123@gmail.com>
2018-02-11 01:27:24 -0600

    Starting to render received JSON in Vue component.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,83 +1,9 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
+    {{typeof(jsonData)}}
+    <ul v-if="typeof(jsonData) === 'object'">
+      <li v-for="prop in Object.keys(jsonData)" v-bind:prop="prop" :key="prop">
+        {{prop}}
       </li>
     </ul>
   </div>
@@ -89,18 +15,13 @@ export default {
   created () {
     window.vueBus.$on('jsonLoaded', json => {
       console.log(json)
+      this.jsonData = json
     })
-    console.log('Created')
   },
   data () {
-    console.log('Data')
     return {
-      msg: 'Message'
-    }
-  },
-  events: {
-    jsonLoaded (arg) {
-      this.msg = JSON.stringify(arg)
+      jsonData: {
+      }
     }
   }
 }
